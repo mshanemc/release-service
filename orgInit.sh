e77eeadc8d9bb84a803d9becf09b605cb285c06d
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-28 14:52:07 -0600

    30 days

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
 sfdx force:source:push
 sfdx shane:org:reauth --requirecustomdomain
 sfdx force:org:open
